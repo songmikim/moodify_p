45394c44901313bb648841cc6a9110278bb47c8e
rmftp <kyj2001kyj@naver.com>
2025-07-09 11:57:05 +0900

    main page

--- src/main/java/xyz/moodf/main/controllers/MainController.java
+++ src/main/java/xyz/moodf/main/controllers/MainController.java
@@ -2,10 +2,14 @@ package xyz.moodf.main.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.moodf.global.annotations.ApplyCommonController;
 import xyz.moodf.global.libs.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -16,7 +20,14 @@ public class MainController {
     private final Utils utils;
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        List<String> addCommonScript = new ArrayList<>();
+        List<String> addCss = new ArrayList<>();
+        List<String> addCommonCss = new ArrayList<>();
+        addCss.add("main/style");
+        model.addAttribute("addCommonScript", addCommonScript);
+        model.addAttribute("addCss", addCss);
+        model.addAttribute("addCommonCss", addCommonCss);
         return utils.tpl("main/index");
     }
 }
