9c65d719e4b89eafa93c957d3f6d19b17b66a7a6
rmftp <kyj2001kyj@naver.com>
2025-07-25 12:39:47 +0900

    사이드 메뉴 버튼 만들기

--- src/main/resources/static/front/js/common.js
+++ src/main/resources/static/front/js/common.js
@@ -16,4 +16,17 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+    const sideMenu = document.getElementsByClassName('sideMenu');
+    const openBtn = document.getElementsByClassName('openBtn');
+    const closeBtn = document.getElementsByClassName('closeBtn');
+
+    // 메뉴 열기
+    openBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'block';
+    });
+
+    // 메뉴 닫기
+    closeBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'none';
+    });
 });
