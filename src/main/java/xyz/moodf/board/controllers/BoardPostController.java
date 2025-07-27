f210e26f0a8f3b33b791320ab0e84bc470fa06e3
seul <slgian527@gmail.com>
2025-07-27 23:30:31 +0900

    수정 기능(미완)

--- src/main/java/xyz/moodf/board/controllers/BoardPostController.java
+++ src/main/java/xyz/moodf/board/controllers/BoardPostController.java
@@ -13,7 +13,9 @@ import xyz.moodf.board.entities.BoardData;
 import xyz.moodf.board.search.BoardSearch;
 import xyz.moodf.board.services.BoardDataInfoService;
 import xyz.moodf.board.services.BoardDataUpdateService;
+import xyz.moodf.board.services.BoardPermissionService;
 import xyz.moodf.global.annotations.ApplyCommonController;
+import xyz.moodf.global.exceptions.UnAuthorizedException;
 import xyz.moodf.global.libs.Utils;
 import xyz.moodf.global.search.ListData;
 import xyz.moodf.member.libs.MemberUtil;
@@ -31,6 +33,7 @@ public class BoardPostController {
     private final Utils utils;
     private final MemberUtil memberUtil;
     private final BoardDataInfoService InfoService;
+    private final BoardPermissionService permissionService;
     private final BoardConfigInfoService configInfoService;
     private final BoardDataUpdateService updateService;
     private final xyz.moodf.board.validators.BoardDataValidator boardDataValidator;
@@ -72,18 +75,22 @@ public class BoardPostController {
     @PostMapping("/save")
     public String save(@Valid RequestPostBoard form, Errors errors, Model model) {
         String mode = form.getMode();
-        String bid= form.getBid();
+        String bid = form.getBid();
         mode = StringUtils.hasText(mode) ? mode : "register";
+
         commonProcess(bid, mode, model);
 
         boardDataValidator.validate(form, errors);
-        updateService.process(form);
 
+        // 에러가 있으면 저장하지 말고 바로 폼으로 돌아가기
         if (errors.hasErrors()) {
-            return "admin/board/" + mode;
+            return utils.tpl("board/" + mode); // utils.tpl() 사용
         }
 
-        return "redirect:/admin/board";
+        // 에러가 없을 때만 저장
+        updateService.process(form);
+
+        return "redirect:/board/list/" + bid; // {bid} 말고 직접 붙이기
     }
 
     // 게시글 수정
@@ -154,12 +161,80 @@ public class BoardPostController {
 
     /**
      * seq 기준의 공통 처리
-     *  - 게시글 조회가 공통 처리 ...
+     *  - 게시글 조회가 공통 처리
      * @param seq
      * @param mode
      * @param model
      */
     private void commonProcess(Long seq, String mode, Model model) {
+        // 게시글 정보 조회
+        BoardData boardData = InfoService.get(seq);
+        Board board = boardData.getBoard();
 
+        mode = StringUtils.hasText(mode) ? mode : "view";
+
+        List<String> addCommonScript = new ArrayList<>();
+        List<String> addCss = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+        String pageTitle = board.getName(); // 게시판 명
+
+        String skin = board.getSkin();
+        addCss.add("board/style"); // 스킨과 상관없는 공통 스타일
+        addCss.add(String.format("board/%s/style", skin)); // 스킨별 스타일
+
+        addScript.add("board/common"); // 스킨 상관없는 공통 자바스크립트
+
+        if (mode.equals("update")) { // 수정 모드
+            // 수정 폼에 필요한 스크립트와 CSS 추가
+            if (board.isAttachFile() || (board.isImageUpload() && board.isEditor())) {
+                addCommonScript.add("fileManager");
+            }
+
+            if (board.isEditor()) { // 에디터를 사용하는 경우, CKEDITOR5 스크립트를 추가
+                addCommonScript.add("ckeditor5/ckeditor");
+            }
+
+            addScript.add(String.format("board/%s/form", skin)); // 스킨별 양식 관련 자바스크립트
+
+            // 수정 폼 데이터 설정
+            RequestPostBoard form = new RequestPostBoard();
+            form.setMode("update");
+            form.setSeq(boardData.getSeq());
+            form.setBid(board.getBid());
+            form.setGid(boardData.getGid());
+            form.setPoster(boardData.getPoster());
+            form.setSubject(boardData.getSubject());
+            form.setContent(boardData.getContent());
+            form.setNotice(boardData.isNotice());
+            form.setSecret(boardData.isSecret());
+            form.setGuest(boardData.getMember() == null); // 비회원 글인지 확인
+
+            model.addAttribute("requestPostBoard", form);
+            pageTitle = board.getName() + " - 글 수정";
+
+        } else if (mode.equals("view")) { // 조회 모드
+            // 조회수 증가 (별도 서비스에서 처리하는 것이 좋음)
+            addScript.add(String.format("board/%s/view", skin));
+            addScript.add(String.format("board/%s/form", skin));
+            pageTitle = boardData.getSubject() + " - " + board.getName();
+
+            model.addAttribute("canEdit", permissionService.canEdit(boardData));
+            model.addAttribute("canDelete", permissionService.canDelete(boardData));
+            model.addAttribute("isGuest", boardData.getMember() == null);
+
+        } else if (mode.equals("delete")) { // 삭제 모드
+            // 삭제 권한 확인 로직이 여기에 들어갈 수 있음
+            // 실제 삭제 처리는 별도 서비스에서 수행
+            pageTitle = board.getName() + " - 글 삭제";
+        }
+
+        model.addAttribute("addCommonScript", addCommonScript);
+        model.addAttribute("addScript", addScript);
+        model.addAttribute("addCss", addCss);
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("board", board);
+        model.addAttribute("boardData", boardData);
     }
+
+
 }
