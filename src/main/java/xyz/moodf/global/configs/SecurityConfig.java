734cbd9921011e010a4302b6ab2c1688f0ea6855
chltkdwns <137965230+chltkdwns@users.noreply.github.com>
2025-07-28 13:10:52 +0900

    오류수정

--- src/main/java/xyz/moodf/global/configs/SecurityConfig.java
+++ src/main/java/xyz/moodf/global/configs/SecurityConfig.java
@@ -53,13 +53,14 @@ public class SecurityConfig {
          * anyRequest().authenticated() : 회원 전용 페이지가 기본, 일부 페이지 -> 비회원 사이트
          */
         http.authorizeHttpRequests(c -> {
-            c.requestMatchers("/login", "/join", "/board/**", "/diary/**", "/error/**", "/calendar/**", "/uploads/**").permitAll()
-                    .requestMatchers("/front/**", "/mobile/**", "/member/**", "/common/**").permitAll()
-                    .requestMatchers("/api/**").permitAll()
-                    .requestMatchers("/file/upload").permitAll()
-                    //.requestMatchers("/admin/**").hasAuthority("ADMIN")
-                    .requestMatchers("/admin/**").permitAll()
-                   .anyRequest().authenticated();
+                    c.requestMatchers("/login", "/join", "/board/**", "/diary/**", "/error/**", "/calendar/**", "/uploads/**").permitAll()
+                            .requestMatchers("/front/**", "/mobile/**", "/member/**", "/common/**").permitAll()
+                            .requestMatchers("/api/**").permitAll()
+                            .requestMatchers("/file/upload").permitAll()
+                            //.requestMatchers("/admin/**").hasAuthority("ADMIN")
+                            .requestMatchers("/admin/**").permitAll()
+                            .anyRequest().authenticated();
+        });
 
         http.exceptionHandling(c -> {
             c.authenticationEntryPoint(new MemberAuthenticationExceptionHandler()); // 미로그인 상태에서의 인가 실패에 대한 처리
