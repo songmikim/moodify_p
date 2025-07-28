ee47d81618ad857e87b001a3588585adea4d716c
chltkdwns <137965230+chltkdwns@users.noreply.github.com>
2025-07-28 14:42:43 +0900

    비밀번호 찾기 진행 중

--- src/main/java/xyz/moodf/member/controllers/RequestFindPw.java
+++ src/main/java/xyz/moodf/member/controllers/RequestFindPw.java
@@ -1,4 +1,12 @@
 package xyz.moodf.member.controllers;
 
-public class RequestFindPw {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RequestFindPw (
+        @NotBlank @Email
+        String email,
+
+        @NotBlank
+        String name
+){}
