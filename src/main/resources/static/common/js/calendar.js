4bee0bf54c2a4230d49b4d270f97fb29295f2ffd
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-08-03 03:42:55 +0900

    캘린더에서 today 이후는 모두 disabled 설정

--- src/main/resources/static/common/js/calendar.js
+++ src/main/resources/static/common/js/calendar.js
@@ -14,19 +14,21 @@ window.addEventListener("DOMContentLoaded", function() {
 
     /* 캘린더 클릭 이벤트: 날짜 클릭 시 해당 날짜의 일기 페이지로 이동 S */
     const selectDays = document.getElementsByClassName("select-day");
+    const now = new Date();
+    now.setHours(0, 0, 0, 0); // 오늘 날짜 기준으로 시간 제거
+
     for (const el of selectDays) {
-        el.addEventListener("click", function() {
-            if (typeof parent.calendarSelectCallback === 'function') {
-                const {date} = this.dataset;
+        const { date } = el.dataset;
+        const clickedDate = new Date(date);
+        clickedDate.setHours(0, 0, 0, 0);
 
-                const clickedDate = new Date(date);
-                clickedDate.setHours(0, 0, 0, 0);  // 이거 안 하면 오전 9시로 자동 설정됨
-                const now = new Date();
+        if (clickedDate > now) {
+            el.classList.add("disabled");
+        }
 
-                //console.log("date", clickedDate, "now", now);
-                if (clickedDate <= now) {
-                    parent.calendarSelectCallback(date);
-                }
+        el.addEventListener("click", function () {
+            if (typeof parent.calendarSelectCallback === 'function' && clickedDate <= now) {
+                parent.calendarSelectCallback(date);
             }
         });
     }
