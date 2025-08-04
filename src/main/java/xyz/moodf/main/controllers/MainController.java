a502a3c72da230ba702ab2c2da7e85adc4ba63d1
rmftp <kyj2001kyj@naver.com>
2025-08-04 14:44:41 +0900

    메인페이지 접근차단

--- src/main/java/xyz/moodf/main/controllers/MainController.java
+++ src/main/java/xyz/moodf/main/controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.moodf.global.annotations.ApplyCommonController;
 import xyz.moodf.global.libs.Utils;
+import xyz.moodf.member.libs.MemberUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +19,21 @@ import java.util.List;
 public class MainController {
 
     private final Utils utils;
+    private final MemberUtil memberUtil;
 
     @GetMapping
-    public String index(Model model) {
+    public String RedirectwLogin(Model model){
+        common(model);
+        boolean isLogin = memberUtil.isLogin();
+        if (isLogin) {
+            return "redirect:/diary"; // 로그인 되어 있으면 메인 페이지로 리다이렉트
+        } else {
+            return utils.tpl("/login");
+        }
+    }
+
+
+    void common(Model model) {
         List<String> addCommonScript = new ArrayList<>();
         List<String> addCss = new ArrayList<>();
         List<String> addCommonCss = new ArrayList<>();
@@ -28,6 +41,5 @@ public class MainController {
         model.addAttribute("addCommonScript", addCommonScript);
         model.addAttribute("addCss", addCss);
         model.addAttribute("addCommonCss", addCommonCss);
-        return utils.tpl("main/index");
     }
 }
