f1cb4315827fab091a8fc3154f3c165ee6e66859
rmftp <kyj2001kyj@naver.com>
2025-08-04 15:10:30 +0900

    일부 문구 수정

--- src/main/java/xyz/moodf/main/controllers/MainController.java
+++ src/main/java/xyz/moodf/main/controllers/MainController.java
@@ -26,7 +26,7 @@ public class MainController {
         common(model);
         boolean isLogin = memberUtil.isLogin();
         if (isLogin) {
-            return "redirect:/diary"; // 로그인 되어 있으면 메인 페이지로 리다이렉트
+            return "redirect:/diary"; // 로그인 되어 있으면 일기 페이지로 리다이렉트
         } else {
             return utils.tpl("/login");
         }
